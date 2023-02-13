f1ef029310aeee2dcc9e762070e78d1759ef969e
Smolinschi <smolinschi.emma@gmail.com>
2023-02-13 20:26:51 +0200

    cypresProject

---
+++ cypress/e2e/Magento.cy.js
@@ -0,0 +1,11 @@
+let no_results_tearm = "test";
+let search_term = "pants";
+
+describe('search Tests', () => {
+  it("Search for no results"), () => {
+    cy.visit("https://magento.softwaretestingboard.com/");
+    cy.get ("#search").type(no_results_tearm + "{enter}");
+    cy.contains("Your search returned no results.").should("be.visible");
+    cy.contains("Search results for: '" + no_results_tearm +"'").should("be.visible");
+    cy.get("#sorter").should("not.exist")
+  })
